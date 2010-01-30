b60e3bfa0e146579f4018819b073fa4f11a1c943
Andy Armstrong <andy@hexten.net>
2010-01-30 16:01:16 +0000

    Add switcher

---
+++ switch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+branch=$1
+[ -z "$branch" ] && branch="master"
+
+for v in 5.*; do
+  if [ -d $v/.git ] ; then
+    echo "Switching to $v $branch"
+    pushd $v > /dev/null
+    git checkout $branch
+    popd > /dev/null
+  fi
+done
+
+
+# vim:ts=2:sw=2:sts=2:et:ft=sh
+
